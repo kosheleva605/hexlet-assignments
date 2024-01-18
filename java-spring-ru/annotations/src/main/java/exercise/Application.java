93375b4f0850ce38d56a1ba0a038ace6bf264e06
@hexlet/cli <support@hexlet.io>
2024-01-18 10:48:43 +0300

    submit java-spring-ru/annotations

--- java-spring-ru/annotations/src/main/java/exercise/Application.java
+++ java-spring-ru/annotations/src/main/java/exercise/Application.java
@@ -3,16 +3,17 @@ package exercise;
 import exercise.model.Address;
 import exercise.annotation.Inspect;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
         var address = new Address("London", 12345678);
 
         // BEGIN
-
         Arrays.stream(Address.class.getDeclaredMethods()).forEach(it -> {
-            if (it.isAnnotationPresent(Inspect.class))
+            if (it.isAnnotationPresent(Inspect.class)) {
                 System.out.println("Method " + it.getName() + " returns a value of type " + it.getReturnType().getSimpleName());
+            }
         });
         // END
     }
