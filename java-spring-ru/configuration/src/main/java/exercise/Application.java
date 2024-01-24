cb85dcfe738e705bbed2c98e0717719712747ede
@hexlet/cli <support@hexlet.io>
2024-01-24 12:47:27 +0300

    submit java-spring-ru/configuration

--- java-spring-ru/configuration/src/main/java/exercise/Application.java
+++ java-spring-ru/configuration/src/main/java/exercise/Application.java
@@ -26,7 +26,7 @@ public class Application {
     @GetMapping("/admins")
     public List<String> admins() {
 
-        return admins.getName().stream().sorted().toList();
+        return admins.getAdmins().stream().sorted().toList();
     }
     // END
 
